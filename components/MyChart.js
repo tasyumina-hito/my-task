d8884d02debe2c32bbf1855c0e1b506903951828
tasyumina-hito <48035533+tasyumina-hito@users.noreply.github.com>
2022-09-21 14:33:39 +0900

    chakraUI

---
+++ components/MyChart.js
@@ -0,0 +1,7 @@
+const MyChart = () => (
+    <>
+
+    </>
+)
+
+export default MyChart
--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,14 @@
 import '../styles/globals.css'
 import { SessionProvider } from 'next-auth/react';
+import { ChakraProvider } from '@chakra-ui/react';
+
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
     <SessionProvider session={session}>
-      <Component {...pageProps} />
+      <ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
     </SessionProvider>
   );
 }
